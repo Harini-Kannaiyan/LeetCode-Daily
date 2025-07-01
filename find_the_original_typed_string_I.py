3a3871ada9baa218a5973124c517e054811911e9
Harini-Kannaiyan <leetstreak@gmail.com>
2025-07-01 10:39:54 +0530

    file(day-1): solution

---
+++ find_the_original_typed_string_I.py
@@ -0,0 +1,26 @@
+:: Time Complelexity: O(N) | Space: O(1)
+
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        prev = ''
+        count= possibleStringsCount = 1
+        for i in range(len(word)):
+            if prev == word[i]:
+                count+=1
+            else :
+                prev = word[i]
+                possibleStringsCount+=count
+                count = 0
+
+        return possibleStringsCount+count
+
+
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        prev = ''
+        possibleStringsCount =  1
+        for i in range(1, len(word)):
+            if word[i-1] == word[i]:
+                possibleStringsCount+=1
+
+        return possibleStringsCount
