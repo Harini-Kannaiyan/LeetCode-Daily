fa3e09f24aaacbdc7c1f5c0598eecde023ba3637
Sanjeev Yadav <sanjeevyadavcr7@gmail.com>
2025-07-03 21:14:45 +0530

    code: day3

---
+++ Day3/find_the_kth_character_in_string_game_i.py
@@ -0,0 +1,29 @@
+class Solution:
+    #Tc - 2^t , t is the power 1, 2, 3,
+    def kthCharacter(self, k: int) -> str:
+        prevString = 'a'
+        while len(prevString) < k:
+            size = len(prevString)
+            currString =''
+            for ch in prevString:
+                nextChar = chr(ord('a') + ((ord(ch) - ord('a') + 1) % 26))
+                currString+=nextChar
+            prevString+= currString
+        return prevString[k-1] 
+
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        #TC = log(k)
+        ans = 0
+        while k != 1:
+            power = k.bit_length() - 1
+            if (1 << power) == k:
+                power -= 1
+            k -= 1 << power
+            ans += 1
+        return chr(ord("a") + ans)
+
+class Solution:
+    #Tc = O(1)
+    def kthCharacter(self, k: int) -> str:
+        return chr(ord("a")+ (k-1).bit_count())
